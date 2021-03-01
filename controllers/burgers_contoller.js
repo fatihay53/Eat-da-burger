df21f37d4b7f39df10e13befbf63e6f00dd7c6ce
Fatih Ay <f.sultanay@gmail.com>
2021-03-01 07:55:39 -0500

    router controller added

--- controllers/burgers_contoller.js
+++ controllers/burgers_contoller.js
@@ -1,4 +1,32 @@
+var express = require('express');
+var router = express.Router()
+const orm = require("../config/orm");
 
 
+router.get('/', async (req, res)=>{
+    const burger = await orm.selectAll()
+  // console.log(burger)
+    // return it within handlebars
+    res.render('index', { burger: burger });
 
-///
+})
+
+
+router.post('/api/create', async (req, res)=>{
+    let input = req.body
+    let result = await orm.insertOne(input)
+    // console.log(result)
+    res.redirect('/');
+  })
+  
+
+  router.post('/api/burger/:id', async (req, res)=>{
+    const id =  req.params.id 
+  
+    const updateBurger = await orm.updateOne(id)
+  
+    res.redirect('/')
+  })
+
+  
+  module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const orm = require( './config/orm');
+// const orm = require( './config/orm');
 
 const app = express();
 const exphbs = require('express-handlebars');
@@ -20,31 +20,8 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 
 app.set('view engine', 'handlebars');
 
-app.post('/api/create', async (req, res)=>{
-  let input = req.body
-  let result = await orm.insertOne(input)
-  // console.log(result)
-  res.redirect('/');
-})
-
-app.get('/', async (req, res)=>{
-    const burger = await orm.selectAll()
-  // console.log(burger)
-    // return it within handlebars
-    res.render('index', { burger: burger });
-
-})
-app.post('/api/burger/:id', async (req, res)=>{
-  const id =  req.params.id 
-
-  const updateBurger = await orm.updateOne(id)
-
-  res.redirect('/')
-})
-
-// var router = require('./controllers/burgers_controller')
-// app.use('/', router)
-
+var router = require('./controllers/burgers_contoller')
+app.use('/', router)
 
 app.listen(PORT, () =>
   console.log(`Server listening on: http://localhost:${PORT}`)
