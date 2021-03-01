955dd9f483c4de5852bfb40d9adf39851f54792d
Fatih Ay <f.sultanay@gmail.com>
2021-02-28 23:48:22 -0500

    database file fixed as value of boulean

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
 
-INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Burger', 0);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Meat Burger', 0);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', 1);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Double Burger', 1);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Meat Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Burger', true);
